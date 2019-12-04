d27152d3921070c09a0b4fd83fed858df4776686
jarrettkuljis <jarrett@dealerteam.com>
2019-12-04 13:11:27 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var https = require('https');
+var ip = require("ip");
+var testmode = false;
+
+const PORT = process.env.PORT || 5000;
+app.set('view engine','ejs');
+app.use(bodyParser.json({limit: '25mb'}));
+
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
